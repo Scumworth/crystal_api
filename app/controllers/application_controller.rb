b652e45c7cdd7687fc6e6450a385143677e3ad68
Daniel McNinch <danmcninch@gmail.com>
2022-05-21 22:30:59 -0700

    Allows for nil authorization
    enables conditional responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,14 @@ class ApplicationController < ActionController::API
 
   def authenticate_request
     header = request.headers['Authorization']
-    header = header.split(" ").last if header
-    begin
-      decoded_token = JsonWebToken.decode(header)
-      current_user = User.find(decoded_token[:user_id])
-    rescue JWT::DecodeError => e
-      render json: { errors: "Error: #{e.message}" }, status: :unauthorized
+    if header
+      header = header.split(" ").last
+      begin
+        decoded_token = JsonWebToken.decode(header)
+        current_user = User.find(decoded_token[:user_id])
+      rescue JWT::DecodeError => e
+        render json: { errors: "Error: #{e.message}" }, status: :unauthorized
+      end
     end
   end
 end
